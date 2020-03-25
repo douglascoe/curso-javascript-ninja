835aebc555ac5c1c3329e94dd2b02ec9b8fcee8f
Douglas Coé <sr.douglascoe@gmail.com>
2020-03-25 15:25:12 -0300

    Exercicio 18

--- challenge-18/index.js
+++ challenge-18/index.js
@@ -126,7 +126,7 @@ corretas, para depois aplicar no código ;)
   console.log(
     " <h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>".replace(
       /(<(\w+)>)/g,
-      "$1 O texto dentro da tag $2 é $3 . \n"
+      "$1 ( O texto dentro da tag  )  $2 é $3 . \n"
     )
   );
 })();
